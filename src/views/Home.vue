e4c70d4785a86733e0fe6cbfe70f97ddb90ab3d2
Kelly Wu <kelly.wu@edu.uwaterloo.ca>
2019-10-20 15:39:34 -0400

    first page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,30 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>Hi!</h1>
+    <p>this is a fake website</p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+// import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    // HelloWorld
   }
+  // data: function() {
+  //   return {
+  //     variableOne: "hi",
+  //     variableTwo: ["what, hello?"]
+  //   };
+  // }
 };
 </script>
+
+<style lang="scss" scoped>
+h1 {
+  margin-top: 65px;
+}
+</style>
